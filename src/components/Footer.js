c00cce469fa6020c6b300547996a1c6a02577db9
dusan233 <dusan.jovanovicno.4@gmail.com>
2021-03-30 23:16:57 +0200

    displaying social links and copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,9 +8,24 @@ const Footer = () => {
                 <div className="footer__logo">
                     <img src={logo} alt="app logo"/>
                 </div>
-                <div className="footer__links"></div>
+                <div className="footer__links">
+                <ul className="links">
+                    <li className="links__item links__item--facebook">
+                        <a href="https://www.facebook.com/TheDataDB/" target="_blank" rel="noreferrer">
+                            <i class="fab fa-facebook-square"></i>
+                        </a>
+                    </li>
+                    <li className="links__item links__item--twitter">
+                        <a href="https://twitter.com/TheAudioDB" target="_blank" rel="noreferrer">
+                            <i class="fab fa-twitter-square"></i>
+                        </a>
+                    </li>
+                </ul>
+                </div>
             </div>
-            <div className="footer__copyright"></div>
+            <div className="footer__copyright">
+                <p>Copyright - Dusan Jovanovic</p>
+            </div>
         </div>
     )
 }
