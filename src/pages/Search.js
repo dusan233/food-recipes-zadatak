41622e8a428da367f0305fac8bc9147cc0824dc7
dusan233 <dusan.jovanovicno.4@gmail.com>
2021-04-01 21:26:40 +0200

    displaying search results

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,14 +3,28 @@ import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import {getSearchRecipes} from '../actions/search';
 
+import RecipeCard from '../components/Recipe';
+
+const mapStateToProps = (state) => ({
+    meals: state.search.meals,
+    loading: state.search.loading
+})
 const mapDispatchToProps = {
     getSearchRecipes
 }
 
-const Search = ({getSearchRecipes, ...props}) => {
+const Search = ({getSearchRecipes, meals, loading, ...props}) => {
     useEffect(() => {
         getSearchRecipes(props.match.params.recipeName)
     }, [props.match.params.recipeName, getSearchRecipes])
+
+    const renderRecipes = () => {
+        return meals.map(meal => {
+            return (
+                <RecipeCard recipeId={meal.idMeal} recipeImg={meal.strMealThumb} recipeName={meal.strMeal} />
+            )
+        })
+    }
 
     return (
         <div>
@@ -19,10 +33,10 @@ const Search = ({getSearchRecipes, ...props}) => {
             </div>
             <div className="section-devider"></div>
             <div className="search-recipes">
-               
+               {renderRecipes()}
             </div>
         </div>
     )
 }
 
-export default connect(null, mapDispatchToProps)(Search);
+export default connect(mapStateToProps, mapDispatchToProps)(Search);
