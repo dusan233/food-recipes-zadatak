ff9de7a14ead2d2b7feadcd9c5b9ec1e037081a6
dusan233 <dusan.jovanovicno.4@gmail.com>
2021-04-01 21:02:04 +0200

    adding search reducer

---
+++ src/pages/Search.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Search = () => {
+    return (
+        <div>
+            <div className="section-heading">
+                   Search results
+            </div>
+            <div className="section-devider"></div>
+            <div className="search-recipes">
+               
+            </div>
+        </div>
+    )
+}
+
+export default Search
