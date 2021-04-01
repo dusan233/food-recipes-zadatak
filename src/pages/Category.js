a348c19e4f21310d47c80fe18cd2f0e071d2703d
dusan233 <dusan.jovanovicno.4@gmail.com>
2021-04-01 17:27:08 +0200

    displaying recipes on page

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -5,32 +5,58 @@ import { getCategoryMeals } from '../actions/categoryMeals';
 
 const mapStateToProps = (state) => ({
     meals: state.categoryMeals.meals,
+    recomendedMeal: state.categoryMeals.recomendedMeal,
     loading: state.categoryMeals.loading
 })
 const mapDispatchToProps = {
  getCategoryMeals
 }
 
-const Category = ({getCategoryMeals, ...props}) => {
+const Category = ({getCategoryMeals, meals, recomendedMeal, loading, ...props}) => {
 
     useEffect(() => {
         getCategoryMeals(props.match.params.categoryName);
     }, [props.match.params.categoryName, getCategoryMeals]);
 
 
-    
+    const renderRecipes = () => {
+        return meals.map(meal => {
+            return (
+                <div className="recipe-card">
+                <div className="recipe-card__img-container">
+                    <img src={meal.strMealThumb} alt={meal.strMeal}/>
+                </div>
+                <div className="recipe-card__title">
+                    {meal.strMeal}
+                </div>
+            </div>
+            )
+        })
+    }
+
 
     return (
         <div>
             <div className="section-heading">
                     {props.match.params.categoryName}
             </div>
-            <div className="recomended-meal">
-                
+            <div className="recomended-recipe">
+            <h2 style={{marginBottom: "20px"}}>Our Recomendation</h2>
+            <div className="recipe-card-container">
+            <div className="recipe-card">
+                <div className="recipe-card__img-container">
+                    <img src={recomendedMeal.strMealThumb} alt={recomendedMeal.strMeal}/>
+                </div>
+                <div className="recipe-card__title">
+                    {recomendedMeal.strMeal}
+                </div>
+            </div>
+            </div>
+            
             </div>
             <div className="section-devider"></div>
             <div className="category-recipes">
-
+               {renderRecipes()}
             </div>
         </div>
     )
