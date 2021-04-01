1e107047fb457f3719bc17ebf8c623bd959959a0
dusan233 <dusan.jovanovicno.4@gmail.com>
2021-04-01 22:18:05 +0200

    fetching random meal together with search results

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import authReducer from './authentication';
 import categoreisReducer from "./categories";
 import categoryMealsReducer from './categoryMeals';
 import searchReducer from './search';
+import randomMealReducer from './randomMeal';
 
 export default combineReducers({
     authanticated: authReducer,
     categories: categoreisReducer,
     categoryMeals: categoryMealsReducer,
-    search: searchReducer
+    search: searchReducer,
+    randomMeal: randomMealReducer
 })
