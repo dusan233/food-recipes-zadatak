ff6347b1372a01fbe132ccf039434010c779a7f4
dusan233 <dusan.jovanovicno.4@gmail.com>
2021-04-02 15:05:56 +0200

    displaying my meals

--- src/pages/MyMeals.js
+++ src/pages/MyMeals.js
@@ -1,13 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { useEffect } from 'react';
 
-const MyMeals = () => {
+import { connect } from "react-redux";
+import { getMyMeals } from '../actions/myMeals';
+import RecipeCard from '../components/Recipe';
+
+const mapStateToProps = state => ({
+    loading: state.myMeals.loading,
+    meals: state.myMeals.meals
+})
+const mapDispatchToProps = {
+    getMyMeals
+}
+
+const MyMeals = ({getMyMeals, meals, loading}) => {
+    useEffect(() => {
+        getMyMeals()
+    }, [getMyMeals])
+
+    const renderRecipes = () => {
+        return meals.map((meal, i) => {
+            return (
+                <RecipeCard key={i}  recipeId={meal.idMeal} recipeImg={meal.strMealThumb} recipeName={meal.strMeal} />
+            )
+        })
+    }
+
     return (
         <div>
             <div className="section-heading">
                     My Meals
                 </div>
+                {loading ? <div className="loading-container">Loading...</div> :<React.Fragment>
+                <div className="my-meals-recipes">
+                    {renderRecipes()}
+                </div>
+                    </React.Fragment>}
+               
         </div>
     )
 }
 
-export default MyMeals
+export default connect(mapStateToProps, mapDispatchToProps)(MyMeals)
