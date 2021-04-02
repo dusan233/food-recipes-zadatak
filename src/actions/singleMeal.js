af8897db5ddfcd44437c0c92badcd9298da4375d
dusan233 <dusan.jovanovicno.4@gmail.com>
2021-04-02 21:38:07 +0200

    fetching random meals of same category and listing them

--- src/actions/singleMeal.js
+++ src/actions/singleMeal.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-
+import { batch } from 'react-redux';
+import { setSimilarMeals } from './similarMeals';
 export const fetchSingleMeal = () => ({
     type: "FETCH_SINGLE_MEAL"
 })
@@ -13,8 +14,24 @@ export const getSingleMealData = (recipeId) => {
         dispatch(fetchSingleMeal())
         try {
             const recipeDetailsRes = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`);
-            console.log(recipeDetailsRes);
-            dispatch(fetchSingleMealSuccess(recipeDetailsRes.data.meals[0]))
+            
+            const similarMealsRes = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${recipeDetailsRes.data.meals[0].strCategory}`);
+            const startIndex = similarMealsRes.data.meals.length > 3 ? Math.floor(Math.random() * (similarMealsRes.data.meals.length - 3)) : 0;
+            
+            const similarMealsFullDataRes = await Promise.all([
+                axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${similarMealsRes.data.meals[startIndex].idMeal}`),
+                axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${similarMealsRes.data.meals[startIndex + 1].idMeal}`),
+                axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${similarMealsRes.data.meals[startIndex + 2].idMeal}`)
+            ])
+            const similarMealsFullData = [];
+            console.log(startIndex, similarMealsRes);
+            similarMealsFullDataRes.forEach(res => {
+                similarMealsFullData.push(res.data.meals[0]);
+            })
+            batch(() => {
+                dispatch(fetchSingleMealSuccess(recipeDetailsRes.data.meals[0]));
+                dispatch(setSimilarMeals(similarMealsFullData));
+            })
         }catch(err) {
             console.log(err);
         }
--- src/pages/SingleMeal.js
+++ src/pages/SingleMeal.js
@@ -5,16 +5,18 @@ import { getSingleMealData } from '../actions/singleMeal';
 import RecipeHeader from '../components/RecipeHeader';
 import RecipeInfo from '../components/RecipeInfo';
 import IngredientList from '../components/IngredientsList';
+import RecipeCard from '../components/Recipe';
 
 const mapStateToProps = state => ({
     loading: state.singleMeal.loading,
-    meal: state.singleMeal.meal
+    meal: state.singleMeal.meal,
+    similarMeals: state.similarMeals
 })
 const mapDispatchToProps = {
     getSingleMealData
 }
 
-const SingleMeal = ({getSingleMealData, meal,loading,  ...props}) => {
+const SingleMeal = ({getSingleMealData, meal,loading, similarMeals,  ...props}) => {
     useEffect(() => {
         getSingleMealData(props.match.params.recipeId)
     }, [props.match.params.recipeId, getSingleMealData])
@@ -40,6 +42,14 @@ const SingleMeal = ({getSingleMealData, meal,loading,  ...props}) => {
                      <IngredientList meal={meal} />
                
             </div>
+            <div className="section-heading">
+                    Similar Meals
+            </div>
+                <div className="similar-recipes">
+                    {similarMeals.map(meal => {
+                        return <RecipeCard key={meal.idMeal}  recipeArea={meal.strArea} recipeCategory={meal.strCategory} recipeId={meal.idMeal} recipeImg={meal.strMealThumb} recipeName={meal.strMeal} />
+                    })}
+                </div>
                 </React.Fragment>}
             
         </div>
